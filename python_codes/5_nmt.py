5c5fbe6329be363213eed1fe48cd26d454ce841e
Brothereum <o-._.-o@live.com>
2022-11-16 00:25:28 +0530

    Create 5_nmt.py

---
+++ python_codes/5_nmt.py
@@ -0,0 +1,245 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import string
+import nltk
+# create a transformer class for neural machine translation
+class Transformer:
+    # define the constructor
+    def __init__(self, input_dim, output_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, device, max_length=100):
+        # initialize the super class
+        super().__init__()
+        # set the device
+        self.device = device
+        # create a positional encoding layer
+        self.tok_embedding = nn.Embedding(input_dim, hid_dim)
+        # create a positional encoding layer
+        self.pos_embedding = PositionalEncoding(hid_dim, dropout, max_length)
+        # create a transformer encoder layer
+        encoder_layer = nn.TransformerEncoderLayer(hid_dim, n_heads, pf_dim, dropout)
+        # create a transformer encoder
+        self.encoder = nn.TransformerEncoder(encoder_layer, n_layers)
+        # create a decoder layer
+        decoder_layer = nn.TransformerDecoderLayer(hid_dim, n_heads, pf_dim, dropout)
+        # create a transformer decoder
+        self.decoder = nn.TransformerDecoder(decoder_layer, n_layers)
+        # create a linear layer
+        self.out = nn.Linear(hid_dim, output_dim)
+        # create a dropout layer
+        self.dropout = nn.Dropout(dropout)
+    # define the forward function
+    def forward(self, src, trg, src_mask, trg_mask):
+        # create a source embedding
+        src = self.dropout(self.tok_embedding(src) * np.sqrt(self.hid_dim))
+        # create a target embedding
+        trg = self.dropout(self.tok_embedding(trg) * np.sqrt(self.hid_dim))
+        # create a source embedding
+        src = self.pos_embedding(src)
+        # create a target embedding
+        trg = self.pos_embedding(trg)
+        # encode the source
+        enc_src = self.encoder(src, src_mask)
+        # decode the target
+        output = self.decoder(trg, enc_src, trg_mask, src_mask)
+        # create a prediction
+        output = self.out(output)
+        # return the result
+        return output
+# create a positional encoding class
+class PositionalEncoding(nn.Module):
+    # define the constructor
+    def __init__(self, hid_dim, dropout, max_length=100):
+        # initialize the super class
+        super().__init__()
+        # create a dropout layer
+        self.dropout = nn.Dropout(dropout)
+        # create a positional encoding
+        pe = torch.zeros(max_length, hid_dim)
+        # create a position
+        position = torch.arange(0, max_length).unsqueeze(1)
+        # create a division term
+        div_term = torch.exp(torch.arange(0, hid_dim, 2) * (-np.log(10000.0) / hid_dim))
+        # create a positional encoding
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # create a positional encoding
+        pe[:, 1::2] = torch.cos(position * div_term)
+        # create a positional encoding
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        # register a buffer
+        self.register_buffer('pe', pe)
+    # define the forward function
+    def forward(self, x):
+        # add the positional encoding
+        x = x + self.pe[:x.size(0), :]
+        # return the result
+        return self.dropout(x)
+# create a function to create a mask
+def create_mask(src, trg):
+    # create a source mask
+    src_mask = (src != SRC.vocab.stoi['<pad>']).unsqueeze(1).unsqueeze(2)
+    # create a target mask
+    trg_mask = (trg != TRG.vocab.stoi['<pad>']).unsqueeze(1).unsqueeze(3)
+    # create a target mask
+    trg_mask = trg_mask & subsequent_mask(trg.shape[1]).type_as(trg_mask.data)
+    # return the result
+    return src_mask, trg_mask
+# create a function to create a subsequent mask
+def subsequent_mask(size):
+    # create a subsequent mask
+    subsequent_mask = np.triu(np.ones((1, size, size)), k=1).astype('uint8')
+    # return the result
+    return torch.from_numpy(subsequent_mask) == 0
+# create a function to initialize the weights
+def init_weights(m):
+    # check if the model is linear
+    for name, param in m.named_parameters():
+        # check if the name contains 'weight'
+        if 'weight' in name:
+            # initialize the weights
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        # check if the name contains 'bias'
+        else:
+            # initialize the bias
+            nn.init.constant_(param.data, 0)
+# create a function to count the number of trainable parameters
+def count_parameters(model):
+    # return the result
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+# create a function to create a batch
+def create_batch(batch):
+    # create a source
+    src = batch.src
+    # create a target
+    trg = batch.trg
+    # return the result
+    return src, trg
+# create a function to train the model
+def train(model, iterator, optimizer, criterion, clip):
+    # set the model to train mode
+    model.train()
+    # initialize the loss
+    epoch_loss = 0
+    # loop over the batches
+    for i, batch in enumerate(iterator):
+        # create a source and target
+        src, trg = create_batch(batch)
+        # create a source mask and target mask
+        src_mask, trg_mask = create_mask(src, trg)
+        # set the gradients to zero
+        optimizer.zero_grad()
+        # create a prediction
+        output = model(src, trg, src_mask, trg_mask)
+        # reshape the output
+        output = output[1:].view(-1, output.shape[-1])
+        # reshape the target
+        trg = trg[1:].view(-1)
+        # calculate the loss
+        loss = criterion(output, trg)
+        # calculate the gradients
+        loss.backward()
+        # clip the gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        # update the parameters
+        optimizer.step()
+        # update the loss
+        epoch_loss += loss.item()
+    # return the result
+    return epoch_loss / len(iterator)
+# create a function to evaluate the model
+def evaluate(model, iterator, criterion):
+    # set the model to evaluation mode
+    model.eval()
+    # initialize the loss
+    epoch_loss = 0
+    # loop over the batches
+    with torch.no_grad():
+        # loop over the batches
+        for i, batch in enumerate(iterator):
+            # create a source and target
+            src, trg = create_batch(batch)
+            # create a source mask and target mask
+            src_mask, trg_mask = create_mask(src, trg)
+            # create a prediction
+            output = model(src, trg, src_mask, trg_mask)
+            # reshape the output
+            output = output[1:].view(-1, output.shape[-1])
+            # reshape the target
+            trg = trg[1:].view(-1)
+            # calculate the loss
+            loss = criterion(output, trg)
+            # update the loss
+            epoch_loss += loss.item()
+    # return the result
+    return epoch_loss / len(iterator)
+# create a function to predict the model
+def predict(model, iterator, criterion):
+    # set the model to evaluation mode
+    model.eval()
+    # initialize the loss
+    epoch_loss = 0
+    # loop over the batches
+    with torch.no_grad():
+        # loop over the batches
+        for i, batch in enumerate(iterator):
+            # create a source and target
+            src, trg = create_batch(batch)
+            # create a source mask and target mask
+            src_mask, trg_mask = create_mask(src, trg)
+            # create a prediction
+            output = model(src, trg, src_mask, trg_mask)
+            # reshape the output
+            output = output[1:].view(-1, output.shape[-1])
+            # reshape the target
+            trg = trg[1:].view(-1)
+            # calculate the loss
+            loss = criterion(output, trg)
+            # update the loss
+            epoch_loss += loss.item()
+    # return the result
+    return epoch_loss / len(iterator)
+# create a function to calculate the accuracy
+def calculate_accuracy(output, target):
+    # calculate the accuracy
+    acc = (output.argmax(1) == target).float().mean()
+    # return the result
+    return acc
+# create a function to calculate the accuracy
+# create a main function
+def main():
+    # create a dataset
+    dataset = Multi30k(split=('train', 'valid', 'test'), language_pair=('de', 'en'))
+    # create a train dataset
+    train_dataset = dataset[0]
+    # create a validation dataset
+    valid_dataset = dataset[1]
+    # create a test dataset
+    test_dataset = dataset[2]
+    # create a source field
+    SRC = Field(tokenize=tokenize_de, init_token
+                ='<sos>', eos_token
+                ='<eos>', lower=True)
+    # create a target field
+    TRG = Field(tokenize=tokenize_en, init_token
+
+                ='<sos>', eos_token
+                ='<eos>', lower=True)
+    # create a dataset
+    dataset = Multi30k(split=('train', 'valid', 'test'), language_pair=('de', 'en'))
+    # create a train dataset
+    train_dataset = dataset[0]
+    # create a validation dataset
+    valid_dataset = dataset[1]
+    # create a test dataset
+    test_dataset = dataset[2]
+    # create a source field
+    SRC = Field(tokenize=tokenize_de, init_token
+
+                ='<sos>', eos_token
+                ='<eos>', lower=True)
+    
